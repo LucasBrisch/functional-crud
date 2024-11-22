fc01344676c08d03077462a1db4471409dea4e2f
Lucas Brisch <lucasbrisch@gmail.com>
2024-11-22 20:31:39 -0300

    edit and delete buttons working
    the deleting one works perfectly, the adit one on the other hand.... It edits the product as it is supposed to, but I'm ahving trouble making the new image show up after editing something, I will fix this at some point

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use(fileupload());
 
 //importing the mysql module
 const mysql = require('mysql2');
+const fs = require('fs');
 
 //creating a connection to the database
 const connection = mysql.createConnection({
@@ -83,5 +84,85 @@ app.post('/register', (req, res) => {
     res.redirect('/');
 });
 
+//remove route
+app.get('/remove/:ID&:image_url', (req, res) => {
+    let sql = `DELETE FROM products WHERE id = ${req.params.ID}`;
+
+    connection.query(sql, (err, result) => {
+        if (err) {
+            console.log('Error while deleting the product');
+            throw err;
+        }
+        fs.unlink(__dirname + '/images/' + req.params.image_url, (err) => {
+            if (err) {
+                console.log('Error while deleting the image');
+                throw err;
+            }
+            console.log('Product deleted successfully');
+        });
+
+        res.redirect('/');
+    })
+})
+
+app.get('/edit/:ID', (req, res) => {
+    let sql = `SELECT * FROM products WHERE id = ${req.params.ID}`;
+
+    connection.query(sql, (err, result) => {
+        if (err) {
+            console.log('Error while fetching the product');
+            throw err;
+        }
+        res.render('EditForm', {product: result[0]});
+    });
+});
+
+app.post('/editform', (req, res) => {
+
+    //getting the data from the form
+    const name = req.body.name;
+    const price = req.body.price;
+    let id = req.body.identificator;
+    let img_name = req.body.image_url;  //old image name
+    
+
+    try{
+        let NewImg = req.files.img.name;
+        let sql = `UPDATE products SET name = '${name}', price = ${price}, image_url = '${NewImg}' WHERE ID = ${id}`;
+        
+        connection.query(sql, (err, result) => {
+            if (err) {
+                console.log('Error while updating the product');
+                throw err;
+            }
+            req.files.img.mv(__dirname + '/images/' + req.files.img.name);
+            fs.unlink(__dirname + '/images/' + img_name, (err) => {
+                if (err) {
+                    console.log('Error while deleting the image');
+                    throw err;
+                }
+                console.log('Product updated successfully'); });
+
+            
+        });
+
+    } catch (err) {
+        let sql = `UPDATE products SET name = '${name}', price = ${price} WHERE id = ${id}`;
+
+        connection.query(sql, (err, result) => {
+            if (err) {
+                console.log('Error while updating the product');
+                throw err;
+            }
+            console.log('Product updated successfully');
+        })
+
+    }
+
+
+    res.redirect('/');
+    
+})
+    
 // creating a server
 app.listen(3000)
